429e12aced6e74d3188c40b81ec89b4e4bdb2e35
fangyingzhong@caibeitv.com <fangyingzhong@caibeitv.com>
2020-05-31 09:44:23 +0800

    物料

--- goods_material.php
+++ goods_material.php
@@ -3,19 +3,19 @@
   0 => 
   array (
     'type' => '相似推荐',
-    'material_id' => '13256',
+    'material_id' => 13256,
     'desc' => '相似推荐',
   ),
   1 => 
   array (
     'type' => '含全部商品',
-    'material_id' => '6708',
+    'material_id' => 6708,
     'desc' => '猜你喜欢',
   ),
   2 => 
   array (
     'type' => '营销商品库商品（此为具备“私域用户管理-会员运营管理功能”的媒体专用）',
-    'material_id' => '28017',
+    'material_id' => 28017,
     'desc' => '猜你喜欢',
   ),
   3 => 
@@ -45,316 +45,316 @@
   7 => 
   array (
     'type' => '综合',
-    'material_id' => '3756',
+    'material_id' => 3756,
     'desc' => '好券直播',
   ),
   8 => 
   array (
     'type' => '女装',
-    'material_id' => '3767',
+    'material_id' => 3767,
     'desc' => '好券直播',
   ),
   9 => 
   array (
     'type' => '家居家装',
-    'material_id' => '3758',
+    'material_id' => 3758,
     'desc' => '好券直播',
   ),
   10 => 
   array (
     'type' => '数码家电',
-    'material_id' => '3759',
+    'material_id' => 3759,
     'desc' => '好券直播',
   ),
   11 => 
   array (
     'type' => '鞋包配饰',
-    'material_id' => '3762',
+    'material_id' => 3762,
     'desc' => '好券直播',
   ),
   12 => 
   array (
     'type' => '美妆个护',
-    'material_id' => '3763',
+    'material_id' => 3763,
     'desc' => '好券直播',
   ),
   13 => 
   array (
     'type' => '男装',
-    'material_id' => '3764',
+    'material_id' => 3764,
     'desc' => '好券直播',
   ),
   14 => 
   array (
     'type' => '内衣',
-    'material_id' => '3765',
+    'material_id' => 3765,
     'desc' => '好券直播',
   ),
   15 => 
   array (
     'type' => '母婴',
-    'material_id' => ' 3760',
+    'material_id' => 3760,
     'desc' => '好券直播',
   ),
   16 => 
   array (
     'type' => '食品',
-    'material_id' => ' 3761',
+    'material_id' => 3761,
     'desc' => '好券直播',
   ),
   17 => 
   array (
     'type' => '运动户外',
-    'material_id' => ' 3766',
+    'material_id' => 3766,
     'desc' => '好券直播',
+  ),
+  18 => 
+  array (
+    'type' => '综合',
+    'material_id' => 9660,
+    'desc' => '大额券',
   ),
   19 => 
   array (
-    'type' => '综合',
-    'material_id' => '9660',
+    'type' => '女装',
+    'material_id' => 9658,
     'desc' => '大额券',
   ),
   20 => 
   array (
-    'type' => '女装',
-    'material_id' => '9658',
+    'type' => '家居家装',
+    'material_id' => 9655,
     'desc' => '大额券',
   ),
   21 => 
   array (
-    'type' => '家居家装',
-    'material_id' => '9655',
+    'type' => '数码家电',
+    'material_id' => 9656,
     'desc' => '大额券',
   ),
   22 => 
   array (
-    'type' => '数码家电',
-    'material_id' => '9656',
+    'type' => '鞋包配饰',
+    'material_id' => 9648,
     'desc' => '大额券',
   ),
   23 => 
   array (
-    'type' => '鞋包配饰',
-    'material_id' => '9648',
+    'type' => '美妆个护',
+    'material_id' => 9653,
     'desc' => '大额券',
   ),
   24 => 
   array (
-    'type' => '美妆个护',
-    'material_id' => '9653',
+    'type' => '男装',
+    'material_id' => 9654,
     'desc' => '大额券',
   ),
   25 => 
   array (
-    'type' => '男装',
-    'material_id' => '9654',
+    'type' => '内衣',
+    'material_id' => 9652,
     'desc' => '大额券',
   ),
   26 => 
   array (
-    'type' => '内衣',
-    'material_id' => '9652',
+    'type' => '母婴',
+    'material_id' => 9650,
     'desc' => '大额券',
   ),
   27 => 
   array (
-    'type' => '母婴',
-    'material_id' => ' 9650',
+    'type' => '食品',
+    'material_id' => 9649,
     'desc' => '大额券',
   ),
   28 => 
   array (
-    'type' => '食品',
-    'material_id' => ' 9649',
+    'type' => '运动户外',
+    'material_id' => 9651,
     'desc' => '大额券',
   ),
   29 => 
   array (
-    'type' => '运动户外',
-    'material_id' => ' 9651',
-    'desc' => '大额券',
+    'type' => '综合',
+    'material_id' => 13366,
+    'desc' => '高佣榜',
+  ),
+  30 => 
+  array (
+    'type' => '女装',
+    'material_id' => 13367,
+    'desc' => '高佣榜',
   ),
   31 => 
   array (
-    'type' => '综合',
-    'material_id' => '13366',
+    'type' => '家居家装',
+    'material_id' => 13368,
     'desc' => '高佣榜',
   ),
   32 => 
   array (
-    'type' => '女装',
-    'material_id' => '13367',
+    'type' => '数码家电',
+    'material_id' => 13369,
     'desc' => '高佣榜',
   ),
   33 => 
   array (
-    'type' => '家居家装',
-    'material_id' => '13368',
+    'type' => ' 鞋包配饰',
+    'material_id' => 13370,
     'desc' => '高佣榜',
   ),
   34 => 
   array (
-    'type' => '数码家电',
-    'material_id' => '13369',
+    'type' => ' 美妆个护',
+    'material_id' => 13371,
     'desc' => '高佣榜',
   ),
   35 => 
   array (
-    'type' => ' 鞋包配饰',
-    'material_id' => '13370',
+    'type' => ' 男装',
+    'material_id' => 13372,
     'desc' => '高佣榜',
   ),
   36 => 
   array (
-    'type' => ' 美妆个护',
-    'material_id' => '13371',
+    'type' => ' 内衣',
+    'material_id' => 13373,
     'desc' => '高佣榜',
   ),
   37 => 
   array (
-    'type' => ' 男装',
-    'material_id' => '13372',
+    'type' => ' 母婴',
+    'material_id' => 13374,
     'desc' => '高佣榜',
   ),
   38 => 
   array (
-    'type' => ' 内衣',
-    'material_id' => '13373',
+    'type' => ' 食品',
+    'material_id' => 13375,
     'desc' => '高佣榜',
   ),
   39 => 
   array (
-    'type' => ' 母婴',
-    'material_id' => '13374',
+    'type' => ' 运动户外',
+    'material_id' => 13376,
     'desc' => '高佣榜',
   ),
   40 => 
   array (
-    'type' => ' 食品',
-    'material_id' => '13375',
-    'desc' => '高佣榜',
+    'type' => '综合',
+    'material_id' => 3786,
+    'desc' => '品牌券',
   ),
   41 => 
   array (
-    'type' => ' 运动户外',
-    'material_id' => '13376',
-    'desc' => '高佣榜',
+    'type' => '女装',
+    'material_id' => 3788,
+    'desc' => '品牌券',
+  ),
+  42 => 
+  array (
+    'type' => '家居家装',
+    'material_id' => 3792,
+    'desc' => '品牌券',
   ),
   43 => 
   array (
-    'type' => '综合',
-    'material_id' => '3786',
+    'type' => '数码家电',
+    'material_id' => 3793,
     'desc' => '品牌券',
   ),
   44 => 
   array (
-    'type' => '女装',
-    'material_id' => '3788',
+    'type' => ' 鞋包配饰',
+    'material_id' => 3796,
     'desc' => '品牌券',
   ),
   45 => 
   array (
-    'type' => '家居家装',
-    'material_id' => '3792',
+    'type' => ' 美妆个护',
+    'material_id' => 3794,
     'desc' => '品牌券',
   ),
   46 => 
   array (
-    'type' => '数码家电',
-    'material_id' => '3793',
+    'type' => ' 男装',
+    'material_id' => 3790,
     'desc' => '品牌券',
   ),
   47 => 
   array (
-    'type' => ' 鞋包配饰',
-    'material_id' => ' 3796',
+    'type' => ' 内衣',
+    'material_id' => 3787,
     'desc' => '品牌券',
   ),
   48 => 
   array (
-    'type' => ' 美妆个护',
-    'material_id' => ' 3794',
+    'type' => ' 母婴',
+    'material_id' => 3789,
     'desc' => '品牌券',
   ),
   49 => 
   array (
-    'type' => ' 男装',
-    'material_id' => ' 3790',
+    'type' => ' 食品',
+    'material_id' => 3791,
     'desc' => '品牌券',
   ),
   50 => 
   array (
-    'type' => ' 内衣',
-    'material_id' => ' 3787',
+    'type' => ' 运动户外',
+    'material_id' => 3795,
     'desc' => '品牌券',
   ),
   51 => 
   array (
-    'type' => ' 母婴',
-    'material_id' => ' 3789',
-    'desc' => '品牌券',
+    'type' => '母婴_备孕',
+    'material_id' => 4040,
+    'desc' => '母婴主题',
   ),
   52 => 
   array (
-    'type' => ' 食品',
-    'material_id' => ' 3791',
-    'desc' => '品牌券',
+    'type' => '母婴_0至6个月',
+    'material_id' => 4041,
+    'desc' => '母婴主题',
   ),
   53 => 
   array (
-    'type' => ' 运动户外',
-    'material_id' => ' 3795',
-    'desc' => '品牌券',
+    'type' => '母婴_4至6岁',
+    'material_id' => 4044,
+    'desc' => '母婴主题',
+  ),
+  54 => 
+  array (
+    'type' => '母婴_7至12个月',
+    'material_id' => 4042,
+    'desc' => '母婴主题',
   ),
   55 => 
   array (
-    'type' => '母婴_备孕',
-    'material_id' => '4040',
+    'type' => '母婴_1至3岁',
+    'material_id' => 4043,
     'desc' => '母婴主题',
   ),
   56 => 
   array (
-    'type' => '母婴_0至6个月',
-    'material_id' => '4041',
+    'type' => '母婴_7至12岁',
+    'material_id' => 4045,
     'desc' => '母婴主题',
   ),
   57 => 
-  array (
-    'type' => '母婴_4至6岁',
-    'material_id' => '4044',
-    'desc' => '母婴主题',
-  ),
-  58 => 
-  array (
-    'type' => '母婴_7至12个月',
-    'material_id' => '4042',
-    'desc' => '母婴主题',
-  ),
-  59 => 
-  array (
-    'type' => '母婴_1至3岁',
-    'material_id' => '4043',
-    'desc' => '母婴主题',
-  ),
-  60 => 
-  array (
-    'type' => '母婴_7至12岁',
-    'material_id' => '4045',
-    'desc' => '母婴主题',
-  ),
-  61 => 
   array (
     'type' => '有好货',
     'material_id' => 4092,
     'desc' => '有好货',
   ),
-  62 => 
+  58 => 
   array (
     'type' => '潮流范',
     'material_id' => 4093,
     'desc' => '潮流范',
   ),
-  63 => 
+  59 => 
   array (
     'type' => '特惠',
     'material_id' => 4094,
